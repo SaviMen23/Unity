f609b3ee51b58085c59cecae4c36153a2537a98f
SaviMen23 <gdakk417@gmail.com>
2026-02-13 20:50:13 +0300

    deleted

--- project/Assets/Scripts/TransformationBase.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class TransformationBase : MonoBehaviour
-{
-    [SerializeField] protected float Speed;
-}
