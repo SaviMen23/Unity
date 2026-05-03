7e25f3d916214517b34ba83431cce0b28f662a5e
SaviMen23 <gdakk417@gmail.com>
2026-05-03 11:45:53 +0300

    UI2 3

--- UI2/Assets/scriptes/VolumeChanger.cs
+++ UI2/Assets/scriptes/VolumeChanger.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeChanger : MixerGroupBase
 {
+    [SerializeField] private Slider _slider;
+
+    private void Awake()
+    {
+        _slider.onValueChanged.AddListener(ChangeVolume);
+    }
+
     public void ChangeVolume(float volume)
     {
         MixerGroup.audioMixer.SetFloat(ExposedParameterName, Mathf.Lerp(MinDecibel, MaxDecibel, volume));
--- UI2/Assets/scriptes/volumeSwitch.cs
+++ UI2/Assets/scriptes/volumeSwitch.cs
@@ -1,5 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 public class volumeSwitch : MixerGroupBase
 {
+    [SerializeField] private Toggle _toggle;
+
+    private void Awake()
+    {
+        _toggle.onValueChanged.AddListener(Mute);
+    }
+
     public void Mute(bool isWork)
     {
         MixerGroup.audioMixer.SetFloat(ExposedParameterName, isWork ? MinDecibel : MaxDecibel);
